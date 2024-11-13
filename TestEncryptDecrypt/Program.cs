085ffcba9158394f5844cab9d2d75774770af97d
anhhiu <dtc2054801360068@ictu.edu.vn>
2024-11-13 10:51:42 +0700

    Add project files.

---
+++ TestEncryptDecrypt/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using TestEncryptDecrypt;
+
+
+
+string e = EncryptDecrypt.EncryptString("nhập mã hóa vào đây");
+string d = EncryptDecrypt.DecryptString(e);
+
+Console.WriteLine(e+"\n");
+Console.WriteLine(d);
+
